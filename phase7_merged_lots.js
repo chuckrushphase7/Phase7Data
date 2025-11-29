45b20d3145fcb55e0c455edd96a5d04ba72e26e3
chuckrushphase7 <chuck.rush@gmail.com>
2025-11-29 12:12:04 -0500

    Update lot data / Christmas changes

--- phase7_merged_lots.js
+++ phase7_merged_lots.js
@@ -58,7 +58,7 @@ const phaseResidentsData = [
     "lotNumber": 1700,
     "homeTypeStyle": "Beach, Coconut",
     "contractStatus": "UC",
-    "primaryName": null,
+    "primaryName": "owner 1700",
     "secondaryName": null,
     "originCityState": null,
     "address": "181 Fins Up Ct.",
