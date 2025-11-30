bd4e7d5c0afd70e9b55e8b77ac57e2cda0b3be3f
chuckrushphase7 <chuck.rush@gmail.com>
2025-11-30 16:44:06 -0500

    Auto-build: Sun 11/30/2025 16:44:05.76

--- apk_info.js
+++ apk_info.js
@@ -1,3 +1,3 @@
 // AUTO-GENERATED FILE — DO NOT EDIT
 // Last build date for Phase7Residents.apk
-const APK_LAST_UPDATED = "Sun 11/30/2025 16:39:54.98";
+const APK_LAST_UPDATED = "Sun 11/30/2025 16:43:57.30";
